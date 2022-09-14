8c922b3a07783b9afcaf424d8370bd72deb83e2e
Victor <victorblessed9@gmail.com>
2022-09-14 18:06:05 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""This is the user class"""
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    reviews = relationship('Review', cascade='all, delete', backref='user')
-    places = relationship('Place', cascade='all, delete', backref='user')
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", passive_deletes=True, backref="user")
+        reviews = relationship("Review", passive_deletes=True, backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
